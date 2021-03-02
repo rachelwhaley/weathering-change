654df7b88b54eb5752e1a244c35bd7b2a7b81a4d
Rachel Whaley <rachelwhaley24@gmail.com>
2021-03-01 20:15:23 -0800

    adding zip codes

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 STATION_COLS = [(0,11), (12,20), (21,30), (31,37), (38,40),
                 (41,70), (72,75), (76,79), (80,86), (87,None)]
+ZIP_COLS = [(0,11), (12,17), (18,None)]
 
 # blanks are -9999
 # P = provisinal, filter out?
@@ -13,22 +14,25 @@ STATION_COLS = [(0,11), (12,20), (21,30), (31,37), (38,40),
 # feed in all the files (13 weather files)
 # all files need headers
 
-
-# read in the stations
-# process as fixed width
-# get zip codes from zips file
-def station_config(station_file): # , zip_file):
+# process the fixed width files
+def station_config(station_file, zip_file):
     # column definitions at https://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/readme.txt
     stations_df = pd.read_fwf(station_file, 
                               colspecs = STATION_COLS, 
                               names = ['station_code', 'lat', 'lon', 'elevation', 'state', 'name', 'gsn_flag', 'hcn_flag', 'wmo_id', 'method'])
     print(stations_df.shape)
     print(stations_df.iloc[:3])
-    return 
+
+    zip_df = pd.read_fwf(zip_file, colspecs = ZIP_COLS, names = ['station_code', 'zip_code', 'city'])
+    print(zip_df.shape)
+    print(zip_df.iloc[:3])
+    result_df = pd.merge(stations_df, zip_df, on='station_code', how='left')
+    print(result_df.iloc[:20])
+    return result_df
 
 # read in each weather attribute
 
 if __name__=='__main__':
     if len(sys.argv) == 0:
         print("Usage: ")
-    station_config(sys.argv[1])
+    station_config(sys.argv[1], sys.argv[2])
