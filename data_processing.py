eee085b92679e08af08043816ae5310b6d7f7fa6
Rachel Whaley <rachelwhaley24@gmail.com>
2021-05-04 21:53:35 -0700

    Initial weather facts table with temps

--- data_processing.py
+++ data_processing.py
@@ -38,22 +38,15 @@ HLY_TEMP_COLS = [(0,11),(12,14),(15,17), # station_code', 'month', 'day'
 
 # process the fixed width files
 
-def station_config(station_file, zip_file):
+def station_config(station_file, zip_file,selected_stations):
     # column definitions at https://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/readme.txt
     stations_df = pd.read_fwf(station_file, 
                               colspecs = STATION_COLS, 
                               names = ['station_code', 'lat', 'lon', 'elevation', 'state', 'name', 'gsn_flag', 'hcn_flag', 'wmo_id', 'method'])
-    print(stations_df.shape)
-    print(stations_df.iloc[:3])
+    # print(stations_df.shape)
+    # print(stations_df.iloc[:3])
 
     # filter for only the stations dad selected
-    include_file = open('StationInclude.csv', 'r')
-    include_list = csv.reader(include_file)
-    selected_stations = []
-
-    for item in include_list:
-        selected_stations.append(item[0])
-
     stations_filtered = stations_df[stations_df['station_code'].isin(selected_stations)]
     stations_filtered = stations_filtered.reset_index(drop=True)
 
@@ -90,22 +83,94 @@ def weather_temp_config(stations_df, weather_file):
     # TO ADD THE STATIONS BACK IN 
     # new_df = pd.merge(stations_df, this_weather_df, on='station_code', how='left')
 
-    
-
     print(this_weather_filtered.shape)
     # print(this_weather_df.iloc[:20])
 
     return this_weather_filtered
 
-# read in each weather attribute
+# construct weather-hour table (THIS IS THE NEW ONE)
+def weather_hour_config(selected_stations):
+    # takes in all the weather files and list of stations to filter by
+    # returns StationCode, DateHourCode, and 13 weather facts
+
+    # filter out the stations we're not using
+    this_weather_df = pd.read_fwf('hly-temp-normal.txt', 
+                                  colspecs = HLY_TEMP_COLS, 
+                                  names = ['station_code', 'month', 'day', '7','8','9','10','11','12','13','14','15','16','17','18','19'])
+    this_weather_filtered = this_weather_df[this_weather_df['station_code'].isin(selected_stations)]
+    this_weather_filtered = this_weather_filtered.reset_index(drop=True)
+
+    print(this_weather_filtered.shape)
+
+    # preprocessing of data
+    this_weather_filtered.replace(to_replace = -9999, value=None, inplace=True)
+    # calculate average for the day
+    this_weather_filtered.iloc[:, 3:].div(10)
+    # this_weather_filtered['average'] = this_weather_filtered.iloc[:, 3:].astype(float).mean(axis=1)/10
+
+    # start with 1 file to construct the DateHourCodes and grab the weather facts
+    hour_date_dicts = []
+    for index, row in this_weather_filtered.iterrows() :
+        date_code = str(row['month']) + str(row['day'])
+        for hour_code in ['7','8','9','10','11','12','13','14','15','16','17','18','19']:
+            date_hour_code = date_code + hour_code
+            hourly = {'station_code': row['station_code'], 'date_hour_code': date_hour_code, 'temp_normal': row[hour_code]}
+            hour_date_dicts.append(hourly)
+
+    df = pd.DataFrame(hour_date_dicts) 
+    print(df.shape)
+    print(df.iloc[:20])
+
+    # then add in the rest of the files
+    more_weather_files = ['hly-temp-10pctl.txt', 'hly-temp-90pctl.txt']
+    more_weather_dfs = []
+    for file in more_weather_files:
+        this_weather_df = pd.read_fwf(file, 
+                                  colspecs = HLY_TEMP_COLS, 
+                                  names = ['station_code', 'month', 'day', '7','8','9','10','11','12','13','14','15','16','17','18','19'])
+        this_weather_filtered = this_weather_df[this_weather_df['station_code'].isin(selected_stations)]
+        this_weather_filtered = this_weather_filtered.reset_index(drop=True)
+        hour_date_dicts = []
+        for index, row in this_weather_filtered.iterrows() :
+            date_code = str(row['month']) + str(row['day'])
+            for hour_code in ['7','8','9','10','11','12','13','14','15','16','17','18','19']:
+                date_hour_code = date_code + hour_code
+                hourly = {'station_code': row['station_code'], 'date_hour_code': date_hour_code, file.split('.')[0] : row[hour_code]}
+                hour_date_dicts.append(hourly)
+        new_df = pd.DataFrame(hour_date_dicts)
+        more_weather_dfs.append(new_df)
+    
+    for x in more_weather_dfs:
+        new_df = pd.merge(df, x, how='left', left_on=['station_code','date_hour_code'], right_on = ['station_code','date_hour_code'])
+        df = new_df
+    
+    print(new_df.shape)
+    print(new_df.iloc[:20])
+
+    return new_df
+
 
 if __name__=='__main__':
     if len(sys.argv) == 0:
         print("Usage: ")
-    stations = station_config('hly-inventory.txt', 'zipcodes-normals-stations.txt')
+    
+    # filter for only the stations dad selected
+    include_file = open('StationInclude.csv', 'r')
+    include_list = csv.reader(include_file)
+    selected_stations = []
+
+    for item in include_list:
+        selected_stations.append(item[0])
+    
+    # create the stations table
+    stations = station_config('hly-inventory.txt', 'zipcodes-normals-stations.txt', selected_stations)
     stations.to_csv('stations' + str(time.time())  + '.csv')
 
-    
+    # create the weather hour table
+    weather_facts = weather_hour_config(selected_stations)
+    weather_facts.to_csv('weatherfacts' + str(time.time()) + '.csv')
+
+    '''    
     # temps 
     weather_avg = weather_temp_config(stations, 'hly-temp-normal.txt')
     weather_ten_pct = weather_temp_config(stations, 'hly-temp-10pctl.txt')
@@ -127,7 +192,7 @@ if __name__=='__main__':
 
     # wind speed
     wind_avg = weather_temp_config(stations, 'hly-wind-avgspd.txt')
-    wind_clm = weather_temp_config(stations, 'hly-wind-pctclm.txt')
+    wind_clm = weather_temp_config(stations, 'hly-wind-pctclm.txt')'''
 
     # weather.to_csv('output.csv')
 
